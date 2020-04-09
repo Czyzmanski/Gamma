0e5796101c4a5001104f1485076fb3503d14a439
Szymon <szymon.czyzmanski@gmail.com>
2020-04-09 23:30:18 +0200

    Commented valid_player, valid_x and valid_y functions.

--- gamma.c
+++ gamma.c
@@ -15,6 +15,10 @@ struct gamma {
     player_t **players_arr;
 };
 
+/** @name Obszar
+ * Wykorzystanie struktury Find-Union do utrzymania informacji o obszarach gracza.
+ */
+/**@{*/
 static void area_new(field_t *f) {
     field_set_rank(f, 0);
     field_set_parent(f, NULL);
@@ -58,115 +62,43 @@ static bool merge_areas(field_t *f1, field_t *f2) {
         return true;
     }
 }
+/**@}*/
 
-static void board_remove_rows(field_t ***board, uint32_t width,
-                              uint32_t num_of_rows) {
-    for (uint32_t i = 0; i < num_of_rows; i++) {
-        for (uint32_t j = 0; j < width; j++) {
-            field_delete(board[i][j]);
-        }
-        free(board[i]);
-    }
-}
-
-static field_t ***board_new(uint32_t width, uint32_t height) {
-    field_t ***board = malloc(height * sizeof(field_t ***));
-
-    if (board == NULL) {
-        return NULL;
-    }
-    else {
-        bool mem_error = false;
-        uint32_t added_rows = 0;
-
-        for (uint32_t i = 0; i < height && !mem_error; i++, added_rows++) {
-            board[i] = calloc(width, sizeof(field_t **));
-            if (board[i] == NULL) {
-                mem_error = true;
-                board_remove_rows(board, width, added_rows);
-            }
-        }
-
-        if (!mem_error) {
-            return board;
-        }
-        else {
-            free(board);
-            return NULL;
-        }
-    }
-}
-
-static bool gamma_init(gamma_t *g, uint32_t width, uint32_t height,
-                       uint32_t players, uint32_t areas) {
-    g->board = board_new(width, height);
-
-    if (g->board == NULL) {
-        return false;
-    }
-    else {
-        g->players_arr = calloc(players + 1, sizeof(player_t *));
-
-        if (g->players_arr == NULL) {
-            return false;
-        }
-        else {
-            g->width = width;
-            g->height = height;
-            g->players = players;
-            g->areas = areas;
-            g->busy_fields = 0;
-            return true;
-        }
-    }
-}
-
-gamma_t *gamma_new(uint32_t width, uint32_t height,
-                   uint32_t players, uint32_t areas) {
-    if (width == 0 || height == 0 || players == 0 || areas == 0) {
-        return NULL;
-    }
-    else {
-        gamma_t *g = malloc(sizeof(gamma_t));
-        if (g == NULL) {
-            return NULL;
-        }
-        else if (gamma_init(g, width, height, players, areas)) {
-            return g;
-        }
-        else {
-            gamma_delete(g);
-            return NULL;
-        }
-    }
-}
-
-void gamma_delete(gamma_t *g) {
-    if (g != NULL) {
-        if (g->board != NULL) {
-            board_remove_rows(g->board, g->width, g->height);
-            free(g->board);
-        }
-
-        if (g->players_arr != NULL) {
-            for (uint32_t i = 0; i <= g->players; i++) {
-                player_delete(g->players_arr[i]);
-            }
-            free(g->players_arr);
-        }
-
-        free(g);
-    }
-}
-
+/** @brief Sprawdza poprawność numeru gracza.
+ * Sprawdza, czy numer gracza @p player jest liczbą całkowitą dodatnią
+ * nie większą od wartości @p players z funkcji @ref gamma_new.
+ * @param[in] g      – wskaźnik na strukturę przechowującą stan gry,
+ * @param[in] player – numer gracza, liczba dodatnia niewiększa od wartości
+ *                     @p players z funkcji @ref gamma_new.
+ * @return Wartość @p true, jeśli numer gracza jest poprawny,
+ * a @p false w przeciwnym przypadku.
+ */
 static inline bool valid_player(gamma_t *g, int64_t player) {
     return 1 <= player && player <= g->players;
 }
 
+/** @brief Sprawdza poprawność numeru kolumny.
+ * Sprawdza, czy numer kolumny @p x jest liczbą całkowitą nieujemną
+ * mniejszą od wartości @p width z funkcji @ref gamma_new.
+ * @param[in] g – wskaźnik na strukturę przechowującą stan gry,
+ * @param[in] x – numer kolumny, liczba nieujemna mniejsza od wartości
+ *                @p width z funkcji @ref gamma_new,
+ * @return Wartość @p true, jeśli numer kolumny jest poprawny,
+ * a @p false w przeciwnym przypadku.
+ */
 static inline bool valid_x(gamma_t *g, int64_t x) {
     return 0 <= x && x < g->width;
 }
 
+/** @brief Sprawdza poprawność numeru wiersza.
+ * Sprawdza, czy numer wiersza @p y jest liczbą całkowitą nieujemną
+ * mniejszą od wartości @p height z funkcji @ref gamma_new.
+ * @param[in] g – wskaźnik na strukturę przechowującą stan gry,
+ * @param[in] y – numer wiersza, liczba nieujemna mniejsza od wartości
+ *                @p height z funkcji @ref gamma_new,
+ * @return Wartość @p true, jeśli numer wiersza jest poprawny,
+ * a @p false w przeciwnym przypadku.
+ */
 static inline bool valid_y(gamma_t *g, int64_t y) {
     return 0 <= y && y < g->height;
 }
@@ -324,37 +256,6 @@ static void player_update_areas(gamma_t *g, field_t *f) {
     player_merge_adjacent_areas(g, f, x, y + 1);
 }
 
-static void gamma_move_update(gamma_t *g, uint32_t player, uint32_t x, uint32_t y) {
-    if (g->players_arr[player] == NULL) {
-        g->players_arr[player] = player_new(player);
-    }
-
-    g->board[y][x] = field_new(x, y, g->players_arr[player]);
-
-    player_t *p = g->players_arr[player];
-    field_t *f = g->board[y][x];
-
-    g->busy_fields++;
-    player_set_busy_fields(p, player_busy_fields(p) + 1);
-
-    player_update_areas(g, f);
-    neighbours_update_borders(g, f);
-    player_update_borders(g, f, false);
-}
-
-bool gamma_move(gamma_t *g, uint32_t player, uint32_t x, uint32_t y) {
-    if (g == NULL || !valid_player(g, player)) {
-        return false;
-    }
-    else if (!player_move_legal(g, g->players_arr[player], x, y)) {
-        return false;
-    }
-    else {
-        gamma_move_update(g, player, x, y);
-        return true;
-    }
-}
-
 static inline bool valid_busy_field(gamma_t *g, int64_t x, int64_t y) {
     return valid_x(g, x) && valid_y(g, y) && g->board[y][x] != NULL;
 }
@@ -486,6 +387,104 @@ static void old_owner_update_areas(gamma_t *g, player_t *old_owner,
     area_search(g, old_owner, x, y + 1, UNCHECKED);
 }
 
+static void board_remove_rows(field_t ***board, uint32_t width,
+                              uint32_t num_of_rows) {
+    for (uint32_t i = 0; i < num_of_rows; i++) {
+        for (uint32_t j = 0; j < width; j++) {
+            field_delete(board[i][j]);
+        }
+        free(board[i]);
+    }
+}
+
+static field_t ***board_new(uint32_t width, uint32_t height) {
+    field_t ***board = malloc(height * sizeof(field_t ***));
+
+    if (board == NULL) {
+        return NULL;
+    }
+    else {
+        bool mem_error = false;
+        uint32_t added_rows = 0;
+
+        for (uint32_t i = 0; i < height && !mem_error; i++, added_rows++) {
+            board[i] = calloc(width, sizeof(field_t **));
+            if (board[i] == NULL) {
+                mem_error = true;
+                board_remove_rows(board, width, added_rows);
+            }
+        }
+
+        if (!mem_error) {
+            return board;
+        }
+        else {
+            free(board);
+            return NULL;
+        }
+    }
+}
+
+static void board_fill(gamma_t *g, char *board) {
+    uint64_t filled = 0;
+
+    for (int64_t y = g->height - 1; y >= 0; y--, filled++) {
+        for (uint32_t x = 0; x < g->width; x++, filled++) {
+            if (g->board[y][x] == NULL) {
+                board[filled] = '.';
+            }
+            else {
+                board[filled] = player_number(field_owner(g->board[y][x])) + '0';
+            }
+        }
+        board[filled] = '\n';
+    }
+
+    board[filled] = '\0';
+}
+
+static bool gamma_init(gamma_t *g, uint32_t width, uint32_t height,
+                       uint32_t players, uint32_t areas) {
+    g->board = board_new(width, height);
+
+    if (g->board == NULL) {
+        return false;
+    }
+    else {
+        g->players_arr = calloc(players + 1, sizeof(player_t *));
+
+        if (g->players_arr == NULL) {
+            return false;
+        }
+        else {
+            g->width = width;
+            g->height = height;
+            g->players = players;
+            g->areas = areas;
+            g->busy_fields = 0;
+            return true;
+        }
+    }
+}
+
+static void gamma_move_update(gamma_t *g, uint32_t player, uint32_t x, uint32_t y) {
+    if (g->players_arr[player] == NULL) {
+        g->players_arr[player] = player_new(player);
+    }
+
+    g->board[y][x] = field_new(x, y, g->players_arr[player]);
+
+    player_t *p = g->players_arr[player];
+    field_t *f = g->board[y][x];
+
+    g->busy_fields++;
+    player_set_busy_fields(p, player_busy_fields(p) + 1);
+
+    player_update_areas(g, f);
+    neighbours_update_borders(g, f);
+    player_update_borders(g, f, false);
+}
+
 static void gamma_golden_move_update(gamma_t *g, uint32_t player,
                                      uint32_t x, uint32_t y) {
     if (g->players_arr[player] == NULL) {
@@ -509,6 +508,70 @@ static void gamma_golden_move_update(gamma_t *g, uint32_t player,
     player_set_borders(old_owner,
                        player_borders(old_owner) -
                        player_adjacent_free_single_fields(g, old_owner, x, y));
+}
+
+gamma_t *gamma_new(uint32_t width, uint32_t height,
+                   uint32_t players, uint32_t areas) {
+    if (width == 0 || height == 0 || players == 0 || areas == 0) {
+        return NULL;
+    }
+    else {
+        gamma_t *g = malloc(sizeof(gamma_t));
+        if (g == NULL) {
+            return NULL;
+        }
+        else if (gamma_init(g, width, height, players, areas)) {
+            return g;
+        }
+        else {
+            gamma_delete(g);
+            return NULL;
+        }
+    }
+}
+
+void gamma_delete(gamma_t *g) {
+    if (g != NULL) {
+        if (g->board != NULL) {
+            board_remove_rows(g->board, g->width, g->height);
+            free(g->board);
+        }
+
+        if (g->players_arr != NULL) {
+            for (uint32_t i = 0; i <= g->players; i++) {
+                player_delete(g->players_arr[i]);
+            }
+            free(g->players_arr);
+        }
+
+        free(g);
+    }
+}
+
+bool gamma_move(gamma_t *g, uint32_t player, uint32_t x, uint32_t y) {
+    if (g == NULL || !valid_player(g, player)) {
+        return false;
+    }
+    else if (!player_move_legal(g, g->players_arr[player], x, y)) {
+        return false;
+    }
+    else {
+        gamma_move_update(g, player, x, y);
+        return true;
+    }
+}
+
+bool gamma_golden_possible(gamma_t *g, uint32_t player) {
+    if (g == NULL || !valid_player(g, player)) {
+        return false;
+    }
+    else if (g->players_arr[player] == NULL) {
+        return g->busy_fields > 0;
+    }
+    else {
+        player_t *p = g->players_arr[player];
+        return player_golden_possible(p) && player_busy_fields(p) < g->busy_fields;
+    }
 }
 
 bool gamma_golden_move(gamma_t *g, uint32_t player, uint32_t x, uint32_t y) {
@@ -555,37 +618,6 @@ uint64_t gamma_free_fields(gamma_t *g, uint32_t player) {
     }
 }
 
-bool gamma_golden_possible(gamma_t *g, uint32_t player) {
-    if (g == NULL || !valid_player(g, player)) {
-        return false;
-    }
-    else if (g->players_arr[player] == NULL) {
-        return g->busy_fields > 0;
-    }
-    else {
-        player_t *p = g->players_arr[player];
-        return player_golden_possible(p) && player_busy_fields(p) < g->busy_fields;
-    }
-}
-
-static void fill_board(gamma_t *g, char *board) {
-    uint64_t filled = 0;
-
-    for (int64_t y = g->height - 1; y >= 0; y--, filled++) {
-        for (uint32_t x = 0; x < g->width; x++, filled++) {
-            if (g->board[y][x] == NULL) {
-                board[filled] = '.';
-            }
-            else {
-                board[filled] = player_number(field_owner(g->board[y][x])) + '0';
-            }
-        }
-        board[filled] = '\n';
-    }
-
-    board[filled] = '\0';
-}
-
 char *gamma_board(gamma_t *g) {
     if (g == NULL) {
         return NULL;
@@ -594,7 +626,7 @@ char *gamma_board(gamma_t *g) {
         char *board = malloc(((g->width + 1) * g->height + 1) * sizeof(char));
 
         if (board != NULL) {
-            fill_board(g, board);
+            board_fill(g, board);
         }
 
         return board;
