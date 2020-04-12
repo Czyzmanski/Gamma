09dded9e1bb485a91c2031f1caf75bb74a651549
Szymon <szymon.czyzmanski@gmail.com>
2020-04-12 19:53:55 +0200

    Documented mem_alloc_check.h

--- mem_alloc_check.h
+++ mem_alloc_check.h
@@ -3,8 +3,17 @@
 
 #include <stdlib.h>
 
+/**
+ * Kod wyjścia informujący, że podczas działania programu zabrakło pamięci.
+ */
 #define NO_MEMORY_EXIT_CODE 1
 
+/** @brief Sprawdza, czy udało się zaalokować pamięć.
+ * Sprawdza, czy wartość wskaźnika @p object jest równa NULL.
+ * Jeśli tak, kończy działanie programu z kodem wyjścia @p NO_MEMORY_EXIT_CODE,
+ * sygnalizującym, że zabrakło pamięci.
+ * @param[in] object – wskaźnik na zaalokowany obiekt w pamięci.
+ */
 static inline void check_for_successful_alloc(void *object) {
     if (object == NULL) {
         exit(NO_MEMORY_EXIT_CODE);
