2d99269ce267de4d87d0c94d07535a30a59609f7
Szymon <szymon.czyzmanski@gmail.com>
2020-05-06 23:28:53 +0200

    Added player's input handling

--- inter_mode.c
+++ inter_mode.c
@@ -3,12 +3,26 @@
 #include <stdint.h>
 #include <ctype.h>
 #include <string.h>
+#include <termios.h>
+#include <unistd.h>
 
 #include "inter_mode.h"
 #include "gamma.h"
 #include "mem_alloc_check.h"
 
 #define FREE_FIELD '.'
+
+#define IS_ESC(c) (c == '\x1b')
+#define IS_LEFT_SQUARE_BRACKET(c) (c == '[')
+#define IS_CURSOR_UP_CHAR(c) (c == 'A')
+#define IS_CURSOR_DOWN_CHAR(c) (c == 'B')
+#define IS_CURSOR_RIGHT_CHAR(c) (c == 'C')
+#define IS_CURSOR_MOVEMENT_CHAR(c) ('A' <= c && c <= 'D')
+
+#define IS_MOVE_CHAR(c) (c == ' ')
+#define IS_GOLDEN_MOVE_CHAR(c) (c == 'g' || c == 'G')
+#define IS_QUIT_CHAR(c) (c == 'c' || c == 'C')
+#define IS_END_OF_GAME_CHAR(c) (c == 4)
 
 #define ROW_CLEAR_FROM_CURSOR_TO_END "\x1b0dK"
 
@@ -134,15 +148,83 @@ static void inter_mode_print_summary(inter_mode_t *im) {
     }
 }
 
-void inter_mode_handle_input(inter_mode_t *im, uint32_t player) {
+static bool inter_mode_gamma_move(inter_mode_t *im, uint32_t player) {
+    //TODO
+}
 
+static bool inter_mode_gamma_golden_move(inter_mode_t *im, uint32_t player) {
+    //TODO
+}
+
+static int inter_mode_handle_input_when_esc(inter_mode_t *im) {
+    int c;
+    do {
+        c = getchar();
+    } while (IS_ESC(c));
+
+    if (IS_LEFT_SQUARE_BRACKET(c)) {
+        c = getchar();
+
+        if (IS_CURSOR_MOVEMENT_CHAR(c)) {
+            if (IS_CURSOR_UP_CHAR(c)) {
+                inter_mode_move_cursor_up(im);
+            }
+            else if (IS_CURSOR_DOWN_CHAR(c)) {
+                inter_mode_move_cursor_down(im);
+            }
+            else if (IS_CURSOR_RIGHT_CHAR(c)) {
+                inter_mode_move_cursor_right(im);
+            }
+            else {
+                inter_mode_move_cursor_left(im);
+            }
+
+            c = getchar();
+        }
+    }
+
+    return c;
+}
+
+static void inter_mode_handle_input(inter_mode_t *im, uint32_t player,
+                                    bool *end_of_game_char) {
+    int c = getchar();
+    bool quit = false, player_valid_move = false;
+    bool player_golden_possible = gamma_golden_possible(im->g, player);
+
+    while (!player_valid_move && !quit && !(*end_of_game_char)) {
+        if (IS_QUIT_CHAR(c)) {
+            quit = true;
+        }
+        else if (IS_END_OF_GAME_CHAR(c)) {
+            *end_of_game_char = true;
+        }
+        else if (IS_MOVE_CHAR(c) || IS_GOLDEN_MOVE_CHAR(c)) {
+            if (IS_MOVE_CHAR(c)) {
+                player_valid_move = inter_mode_gamma_move(im, player);
+            }
+            else if (player_golden_possible) {
+                player_valid_move = inter_mode_gamma_golden_move(im, player);
+            }
+
+            if (!player_valid_move) {
+                c = getchar();
+            }
+        }
+        else if (IS_ESC(c)) {
+            c = inter_mode_handle_input_when_esc(im);
+        }
+        else {
+            c = getchar();
+        }
+    }
 }
 
 void inter_mode_play_gamma(inter_mode_t *im) {
     uint32_t num_of_players = gamma_players(im->g);
-    bool any_player_possible_move = true, no_exit_code = true;
+    bool any_player_possible_move = true, end_of_game_char = false;
 
-    while (any_player_possible_move && no_exit_code) {
+    while (any_player_possible_move && !end_of_game_char) {
         any_player_possible_move = false;
 
         for (uint64_t player = 1; player <= num_of_players; player++) {
@@ -157,7 +239,7 @@ void inter_mode_play_gamma(inter_mode_t *im) {
 
                 //TODO: move cursor back to starting position
 
-                inter_mode_handle_input(im, player);
+                inter_mode_handle_input(im, player, &end_of_game_char);
             }
         }
     }
@@ -214,7 +296,26 @@ void inter_mode_launch(gamma_t *g) {
     inter_mode_init(&im, g);
 
     inter_mode_print_board(&im);
+
+    struct termios old_term, new_term;
+
+    if (tcgetattr(STDIN_FILENO, &old_term) != 0) {
+        inter_mode_delete_board(&im, im.board_height);
+        exit(EXIT_FAILURE);
+    }
+
+    new_term = old_term;
+    new_term.c_lflag &= ~ECHO;
+    new_term.c_lflag &= ~ICANON;
+
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &new_term) != 0) {
+        inter_mode_delete_board(&im, im.board_height);
+        exit(EXIT_FAILURE);
+    }
+
     inter_mode_play_gamma(&im);
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_term);
 
     inter_mode_delete_board(&im, im.board_height);
 }
